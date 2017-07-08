8679701ae9a3a4b3367b57da0e22ab7a770893aa
Antonio Paternina <apaterninac@tecnocomfenalco.edu.co>
2017-07-07 21:10:17 -0500

    Added react-flexbox-grid for layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 import Button from './Components/Button'
 import iconConfig from './Icons/icon-config.svg';
 import './App.css';
 import logo from './logo.svg';
 import introLogo from './img/background-intro.svg';
+
 /*
 class App extends Component {
   render() {
@@ -25,19 +27,18 @@ class App extends Component {
   render() {
     return (  
       <div className="App">
-        <div className="App-header">
-          <div className="Content-logo">
-            <img src={logo} className="App-logo" alt="logo" />
-          </div>
-          <div className="App-content-button">
+      <Row className="App-header m-r-none m-l-none">
+        <Col lg={2} className="Content-logo">
+          <img src={logo} className="App-logo" alt="logo" />
+        </Col>
+        <Col lg={10} className="App-content-button">
             <Button name={"Acerca de"} />
             <Button name={"Que hacermos?"} />
             <Button name={"Servicios"} />
             <Button name={"Contactos"} />
             <Button name={"Equipo"} />
-          </div>
-          <div></div>
-        </div>
+        </Col>
+      </Row>
         <div className="App-body">
         <div className="circle"></div>
         <div className="street">
