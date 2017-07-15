948987314ffb7dd50078ccb2eff2650bb6ce941b
Antonio Paternina <apaterninac@tecnocomfenalco.edu.co>
2017-07-15 15:01:42 -0500

    Added efect to menu aside

--- src/App.js
+++ src/App.js
@@ -66,6 +66,12 @@ class App extends Component {
   constructor (props) {
       super(props);
       this.scrollToTop = this.scrollToTop.bind(this);
+      this.expandMenu = this.expandMenu.bind(this)
+  }
+
+  expandMenu(e) {
+    let menuAside = document.querySelector('.aside-menu');
+    menuAside.classList.toggle('open');
   }
 
   componentDidMount() {
@@ -79,15 +85,18 @@ class App extends Component {
     });
 
     scrollSpy.update();
-
   }
+
   scrollToTop() {
     animateScroll.scrollToTop();
   }
+
   componentWillUnmount() {
     Events.scrollEvent.remove('begin');
     Events.scrollEvent.remove('end');
   }
+
+
   render () {
     return (
       <div>
@@ -97,16 +106,18 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
           </Col>
           <Col lg={10} md={10} sm={10} className="App-content-link vertical-align hidden-xs hidden-sm">
-             <ul>
-                  <li><Link activeClass="active" className="test1" to="test1" spy={true} smooth={true} duration={500} >Acerca de</Link></li>
-                  <li><Link activeClass="active" className="test6" to="anchor" spy={true} smooth={true} duration={500}>¿Qué hacemos?</Link></li>
-                  <li><Link activeClass="active" className="test7" to="test7" spy={true} smooth={true} duration={durationFn}>Servicios</Link></li>
-                  <li> <a onClick={() => animateScroll.scrollTo(100)}>Contacto</a></li>
-                  <li> <a onClick={() => animateScroll.scrollToBottom()}>Equipo</a></li>
+              <ul>
+                  <li><Link activeClass="active"  to="about" spy={true} smooth={true} duration={500} >Acerca de</Link></li>
+                  <li><Link activeClass="active"  to="whatdo" spy={true} smooth={true} duration={500}>¿Qué hacemos?</Link></li>
+                  <li><Link activeClass="active"  to="services" spy={true} smooth={true} duration={durationFn}>Servicios</Link></li>
+                  <li><Link activeClass="active"  to="contact" spy={true} smooth={true} duration={durationFn}>Contacto</Link></li>
+                  <li><Link activeClass="active"  to="team" spy={true} smooth={true} duration={durationFn}>Equipo</Link></li>
               </ul>
           </Col>
           <Col xs={8} className={"hidden-lg hidden-md hidden-xl content-menu-wrap" }>
-          <i className="material-icons">menu</i>
+            <a onClick={this.expandMenu}>
+              <i className="material-icons">menu</i>
+            </a>
           </Col>
         </Row>
           <div className="App-body">
@@ -114,60 +125,44 @@ class App extends Component {
           </div>
           <div className="App-footer">
           </div>
-        <aside className="side-menu">
-        <a className="close"><i className="material-icons">close</i></a>
+        <aside className="aside-menu">
+        <a className="close" onClick={this.expandMenu}><i className="material-icons">close</i></a>
             <ul>
-                <li><Link activeClass="active" className="test1" to="test1" spy={true} smooth={true} duration={500} >Acerca de</Link></li>
-                <li><Link activeClass="active" className="test6" to="anchor" spy={true} smooth={true} duration={500}>¿Qué hacemos?</Link></li>
-                <li><Link activeClass="active" className="test7" to="test7" spy={true} smooth={true} duration={durationFn}>Servicios</Link></li>
-                <li> <a onClick={() => animateScroll.scrollTo(100)}>Contacto</a></li>
-                <li> <a onClick={() => animateScroll.scrollToBottom()}>Equipo</a></li>
+                <li><Link activeClass="active"  to="about" spy={true} smooth={true} duration={500} >Acerca de</Link></li>
+                <li><Link   to="whatdo" spy={true} smooth={true} duration={500}>¿Qué hacemos?</Link></li>
+                <li><Link   to="services" spy={true} smooth={true} duration={durationFn}>Servicios</Link></li>
+                <li><Link  to="contact" spy={true} smooth={true} duration={durationFn}>Contacto</Link></li>
+                <li><Link  to="team" spy={true} smooth={true} duration={durationFn}>Equipo</Link></li>
             </ul>
         </aside>
 
       </div>
 
 
-        <Element name="test1" className="element" >
-          test 1
+        <Element name="about" className="element" >
+         page one
+        </Element>
+
+        <Element name="whatdo" className="element" >
+          page twoo
+        </Element>
+
+        <Element name="services" className="element" >
+          page 3
+        </Element>
+
+        <Element name="contact" className="element" >
+          page 4
+        </Element>
+
+        <Element name="team" className="element" >
+          page 5
         </Element>
 
         <div id="anchor" className="element">
           test 6 (anchor)
         </div>
 
-        <Link activeClass="active" to="firstInsideContainer" spy={true} smooth={true} duration={250} containerId="containerElement" style={{display:'inline-block', margin: '20px'}}>
-          Go to first element inside container
-        </Link>
-
-        <Link activeClass="active" to="secondInsideContainer" spy={true} smooth={true} duration={250} containerId="containerElement" style={{display:'inline-block', margin: '20px'}}>
-          Go to second element inside container
-        </Link>
-        <Element name="test7" className="element" id="containerElement" style={{
-          position: 'relative',
-          height:'200px',
-          overflow:'scroll',
-          marginBottom: '100px'
-        }}>
-          test 7 (duration and container)
-
-          <Element name="firstInsideContainer" style={{
-            marginBottom: '200px'
-          }}>
-            first element inside container
-          </Element>
-
-          <Element name="secondInsideContainer" style={{
-            marginBottom: '200px'
-          }}>
-            second element inside container
-          </Element>
-        </Element>
-
-
-        <Element id="same" className="element">
-          Two links point to this
-        </Element>
 
         <a onClick={this.scrollToTop}>To the top!</a>
 
